ec67651ff15d7aa3ec0f42b2ed712a8e0f81235e
johnrails <john.davalos@gmail.com>
2015-01-08 14:30:42 -0800

    Fixed some stuff, and added error.go

--- repo.go
+++ repo.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var currentId int
 
-var todos todos
+var todos Todos
 
 // Give us some seed data
 
@@ -26,14 +26,14 @@ func RepoFindTodo(id int) Todo {
 func RepoCreateTodo(t Todo) Todo {
   currentId += 1
   t.Id = currentId
-  todos append(todos, t)
+  todos = append(todos, t)
   return t
 }
 
 func RepoDestroyTodo(id int) error {
   for i, t := range todos{
     if t.Id == id{
-      todos = append(todos[:i], todos[i+1]...)
+      todos = append(todos[:i], todos[i+1:]...)
       return nil
     }
   }
